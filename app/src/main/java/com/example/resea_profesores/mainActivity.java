9b1f48408e7ca90a5fb420502361f18c0d88ccf1
alan-are <alanarellano75@outlook.com>
2025-02-18 15:12:13 -0600

    Descripción de los cambios

---
+++ app/src/main/java/com/example/resea_profesores/mainActivity.java
@@ -0,0 +1,57 @@
+package com.example.resea_profesores;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class mainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button b;
+    private TextView t;
+    private EditText username;
+    private EditText password;
+    private Handler handler = new Handler(Looper.getMainLooper());
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        username = findViewById(R.id.editTextText);
+        password = findViewById(R.id.editTextText2);
+        t = findViewById(R.id.textView6);
+
+        b = findViewById(R.id.button);
+        b.setOnClickListener(this);
+    }
+
+    @SuppressLint("SetTextI18n")
+    @Override
+    public void onClick(View view) {
+        String Username = username.getText().toString(), Password = password.getText().toString();
+        String Verified_User = "Admin", Verified_Password = "123";
+
+        if (Username.equals(Verified_User) && Password.equals(Verified_Password)) {
+            t.setText("Iniciando sesion");
+            handler.postDelayed(() -> t.setText("Iniciando sesion."), 500);
+            handler.postDelayed(() -> t.setText("Iniciando sesion.."), 500);
+            handler.postDelayed(() -> t.setText("Iniciando sesion..."), 1000);
+            handler.postDelayed(() -> {
+                Intent intent = new Intent(mainActivity.this, initialscreen.class);
+                startActivity(intent);
+                t.setText("");
+            }, 1500); //cambios
+        } else {
+            t.setText("Usuario o contraseña incorrectos");
+            handler.postDelayed(() -> t.setText(""), 3000);
+        }
+    }
+}
